f0b23712e76dc1ace7c4035ca0501377e609b609
kenishasequeira <kenishasequeira2001@gmail.com>
2023-05-01 15:37:45 -0400

    Sign Up and Log In functionality connected with Firebase

--- src/app/components/signup_form.jsx
+++ src/app/components/signup_form.jsx
@@ -2,16 +2,54 @@ import styles from '../page.module.css'
 
 export default function Signup_Form(){
     return(
-        <form  className={styles.form} action="/login" method="post">
-          <label for="username">Username: </label>
-          <input type="text" id="name" name="username" /><br/>
-          <label for="email_id">Email ID: </label>
-          <input type="text" id="email_id" name="email_id" /><br/>
-          <label for="password">Password: </label>
-          <input type="text" id="password" name="password" /><br/>
-          <label for="confirm_password">Confirm Password: </label>
-          <input type="password" id="confirm_password" name="confirm_password" /><br/>
-          <button type="submit" className={styles.button_center}>Submit</button>
+        <form className={styles.form}>
+            <div>
+                <label htmlFor="email-address">
+                    Email address:&nbsp;
+                </label>
+                <input
+                    type="email"
+                    label="Email address"
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    placeholder="Email address"
+                />
+            </div>
+
+            <div>
+                <label htmlFor="password">
+                    Password:&nbsp;
+                </label>
+                <input
+                    type="password"
+                    label="Create password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    placeholder="Password"
+                />
+            </div>
+
+            <div>
+                <label htmlFor="confirm-password">
+                    Confirm Password:&nbsp;
+                </label>
+                <input
+                    type="password"
+                    label="Confirm password"
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    required
+                    placeholder="Confirm Password"
+                />
+            </div>
+
+            <button
+                type="submit"
+                onClick={onSubmit}
+                className={styles.button_center}
+            >
+                Sign up
+            </button>
+
         </form>
     );
 }
